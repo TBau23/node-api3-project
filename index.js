f159c2df33fdc464728130885bd38c99ddfc70f7
Tom Bauer <tbauer1898@gmail.com>
2020-09-30 22:46:24 -0500

    Endpoints up.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // code away!
+
+
+// function atGate(req, res, next) {
+//     console.log('At gate outside Moria');
+
+//     next();
+// }
+
+// function auth(req, res, next) {
+//     if (req.url === '/mellon') {
+//         next();
+//     } else {
+//         res.send("You shall not pass!")
+//     };
+// };
+
+// server.get('/mellon', auth, (req, res) => {
+//     console.log('Gate opening ...');
+//     console.log('Inside and safe!');
+//     res.send('Welcome traveler!');
+
+// });
+
+const express = require('express');
+
+const userRouter = require('./users/userRouter');
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/users", userRouter);
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'Server is running just for you my guy...'});
+})
+
+const port = 2000;
+
+server.listen(port, () => {
+    console.log('\n*** Server Running on http://localhost:2000***\n')
+})
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,107 @@
 const express = require('express');
 
+const db = require('./userDb');
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
   // do your magic!
+  db.insert(req.body)
+  .then(user => {
+    res.status(201).json(user)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({
+      message: 'Error adding user ...'
+    })
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
-});
+// router.post('/:id/posts', (req, res) => {
+//   // do your magic!
+// db.insert(req.body)
+//   .then(post => {
+//     res.status(201).json(post)
+//   })
+//   .catch(error => {
+//     console.log(error)
+//     res.status(500).json({
+//       message: 'Error adding post ...'
+//     });
+//   });
+// });
 
 router.get('/', (req, res) => {
   // do your magic!
+  db.get()
+  .then(users => {
+    res.status(200).json({users})
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).json({
+      message: 'Error fetching users ... sorry bro'
+    })
+  })
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  db.getById(req.params.id)
+  .then(user => {
+    res.status(200).json(user)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({ message: "Error fetching user ..."})
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  db.getUserPosts(req.params.id)
+  .then(posts => {
+    res.status(200).json(posts)
+    
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({message : 'Error fetching user posts'});
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  db.remove(req.params.id)
+  .then(count => {
+    if (count > 0) {
+      res.status(200).json({message: 'User removed forever'})
+    } else {
+      res.status(404).json({message: "The post could not be found"})
+    }
+  })
+  .catch(error => {
+    res.status(500).json({ message: "Error deleting user"})
+  })
+
 });
 
 router.put('/:id', (req, res) => {
   // do your magic!
+  const changes = req.body;
+  db.update(req.params.id, changes)
+  .then(user => {
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "The post could not be found"})
+    }
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).json({message: "error updating user"})
+  });
 });
 
 //custom middleware
